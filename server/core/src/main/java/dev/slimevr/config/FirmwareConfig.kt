aaab04aa224daeb748c2ad4072c79f2740ac01c7
tsxc <tsxc_personal@163.com>
2026-06-28 10:24:01 +0800

    Enable custom firmware override by default

--- server/core/src/main/java/dev/slimevr/config/FirmwareConfig.kt
+++ server/core/src/main/java/dev/slimevr/config/FirmwareConfig.kt
@@ -1,7 +1,7 @@
 package dev.slimevr.config
 
 class FirmwareConfig {
-	var enabled: Boolean = false
+	var enabled: Boolean = true
 	var firmwareUrl: String = "https://www.modelscope.cn/datasets/xlxgxlxg/slimevr/resolve/master/firmware.bin"
 	var firmwareDigest: String = ""
 }
